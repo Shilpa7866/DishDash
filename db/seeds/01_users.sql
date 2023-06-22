1cfc70d21c92e76cfbe5b13a529ecab0031aff81
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-22 00:10:10 -0400

    adding users seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+insert into users (uniqueid, name, email, phone_number, password) values ('u100', 'Jackie', 'Jackie@gmail.com', '4123-784-1457', 'Jackie@123');
+insert into users (uniqueid, name, email, phone_number, password) values ('u101', 'Abdella', 'Abdella@gmail.com', '415-774-144', 'Abdella@456');
+insert into users (uniqueid, name, email, phone_number, password) values ('u102', 'Shilpa', 'Shilpa@gmail.com', '897-724-1442', 'Shilpa@789');
+insert into users (uniqueid, name, email, phone_number, password) values ('u103', 'Alice', 'Alice@gmail.com', '647-724-1544', 'Alice@123');
+insert into users (uniqueid, name, email, phone_number, password) values ('u104', 'Taiwo', 'Taiwo@gmail.com', '647-747-4747', 'Taiwo@147');
