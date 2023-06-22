3501d07e7c34c4a690e45e143374d72a49b3ca1b
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-22 00:12:17 -0400

    adding dishes seeds

---
+++ db/seeds/03_dishes.sql
@@ -0,0 +1,14 @@
+insert into dishes (uniqueid, restaurant_id, name, description, availability, photo, price, dietary_information) values 
+('d100', 'r100', 'Butter Chicken', 'Chicken Cooked with  Spices and Butter', '2pm to 9.30pm', 'link_to_butterchicken_photo', '$19.99', '250 cals');
+
+insert into dishes (uniqueid, restaurant_id, name, description, availability, photo, price, dietary_information) values 
+('d101', 'r100', 'Kadai Chicken', 'Chicken Cooked with  Spices and Butter', '2pm to 9.30pm', 'link_to_butterchicken_photo', '$19.99', '250 cals');
+
+insert into dishes (uniqueid, restaurant_id, name, description, availability, photo, price, dietary_information) values 
+('d102', 'r100', 'Ginger Chicken', 'Chicken Cooked with  Spices and Butter', '2pm to 9.30pm', 'link_to_butterchicken_photo', '$19.99', '250 cals');
+
+insert into dishes (uniqueid, restaurant_id, name, description, availability, photo, price, dietary_information) values 
+('d103', 'r100', 'Sweet and Sour Chicken', 'Chicken Cooked with  Spices and Butter', '2pm to 9.30pm', 'link_to_butterchicken_photo', '$19.99', '250 cals');
+
+insert into dishes (uniqueid, restaurant_id, name, description, availability, photo, price, dietary_information) values 
+('d104', 'r100', 'Tandoori Chicken', 'Chicken Cooked with  Spices and Butter', '2pm to 9.30pm', 'link_to_butterchicken_photo', '$19.99', '250 cals');
