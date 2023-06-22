dca40cee593d49ab46109e5789867dc580a15ca9
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-22 00:11:49 -0400

    adding restaurant seeds

---
+++ db/seeds/02_restaurants.sql
@@ -0,0 +1,8 @@
+insert into restaurants (uniqueid, name, description, opening_hours, website, phone_number, address) values 
+('r100', 'Embassy Restaurant', 'South Asian Tasty Dishes', '10am to 10pm MON-SUN', 'www.embassyrestaurant.com', '416-846-7414', '245 Mc Nicol Ave, Scarborough, ON');
+
+insert into restaurants (uniqueid, name, description, opening_hours, website, phone_number, address) values 
+('r101', 'Mc Donalds Restaurant', 'Healthy Fastfoods', '8am to 11pm MON-SUN', 'www.mcdonalds.com', '416-753-1574', '116 Warden Ave E, Vancouver, ON');
+
+insert into restaurants (uniqueid, name, description, opening_hours, website, phone_number, address) values 
+('r102', 'Mandarin Restaurant', 'Chinese Canadian Buffet', '10.30am to 10.30pm MON-SUN', 'www.mandarin.com', '950-741-1968', '2001 Birchmount Rd N, Toronto, ON');
