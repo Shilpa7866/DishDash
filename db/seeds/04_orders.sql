5349eabf888c71015f055dfecc64b91cd95f8e8f
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-22 22:37:35 -0400

    updated

---
+++ db/seeds/04_orders.sql
@@ -0,0 +1,35 @@
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(1, 1, ' 2023-06-20 00:15:00-04', '30 min', '22023-06-20 00:15:00-04', '2023-06-20 00:50:00-04', 2999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(2, 3, ' 2023-06-20 03:15:00-04', '45 min', '2023-06-20 04:00:00-04', '2023-06-20 04:05:00-04', 2999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(2, 3, ' 2023-06-21 04:00:15-04', '30 min', ' 2023-06-21 04:30:15-04', ' 2023-06-21 04:45:15-04', 2999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(3, 2, '2023-06-21 08:00:25-04', '45 min', '2023-06-21 08:45:25-04', '2023-06-21 08:55:48-04', 2999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(3, 1, '2023-06-20 02:00:00-04', '45 min', '2023-06-20 02:45:00-04', '2023-06-20 02:50:00-04', 1999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(1, 3, '2023-06-22 19:10:25-07', '45 min', '2016-06-22 19:55:25-07', '2016-06-22 20:02:13-06', 1999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(3, 4, '2016-06-22 22:08:43-06', '45 min', '2016-06-22 22:53:13-06', '2016-06-22 22:55:26-06', 1999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(4, 4, '2016-06-22 18:00:43-06', '20 min', '2016-06-22 18:20:43-06', '2016-06-22 18:30:25-06', 2499);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(4, 2, '2016-06-22 17:26:25-06', '30 min', '2016-06-22 17:56:25-06', '2016-06-22 18:05:36-06', 1499);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(1, 2, '2016-06-23 14:20:25-06, '30 min', '2016-06-23 14:50:38-06', '2016-06-23 14:55:25-06', 2999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(2, 1, ' 2023-06-20 09:22:25-04', '30 min', '2023-06-20 09:52:25-04', '2023-06-20 09:55:25-04', 2999);
+
+INSERT INTO orders (user_id, restaurant_id, created_at, estimated_time, ready_at, picked_up_at, total_price) VALUES 
+(3, 2, '2023-06-20 03:15:25-04', '30 min', '2023-06-20 03:45:25-04', '2023-06-20 03:55:48-04', 3999);
