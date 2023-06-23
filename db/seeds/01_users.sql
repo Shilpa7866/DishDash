b47aad6afa15a26bafe58c78d9f53be8b312464c
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-22 22:39:59 -0400

    updated

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,7 +4,6 @@ INSERT INTO users (name, email, phone_number, password) VALUES ('Abdella', 'Abde
 
 INSERT INTO users (name, email, phone_number, password) VALUES ('Shilpa', 'Shilpa@gmail.com', '647-637-6969', '$2a$12$8DUP4MOyLrZY9xO1pvHTi.dZ.adLM5WBvEXH2lmD.75nQeolDMz16');
 
-INSERT INTO users (name, email, phone_number, password) VALUES ('Alice', 'Alice@gmail.com', '647-637-6969', '
-$2a$12$ETG6ERsHK4BbU1xfnKBEc.gsJsv18UVLJ6MgrFuTi6OiQ/B3/zaYC');
+INSERT INTO users (name, email, phone_number, password) VALUES ('Alice', 'Alice@gmail.com', '647-637-6969', '$2a$12$ETG6ERsHK4BbU1xfnKBEc.gsJsv18UVLJ6MgrFuTi6OiQ/B3/zaYC');
 
 INSERT INTO users (name, email, phone_number, password) VALUES ('Taiwo', 'Taiwo@gmail.com', '647-637-6969', '$2a$12$SS16IXDhrimtHsprzvOwuOhLs5hfjUmxczHG1AZv.nff8iU.brA1.');
