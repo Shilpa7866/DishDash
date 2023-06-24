56c9ac99ec095bf060b8a0098a3df5a96f27566f
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-23 23:19:40 -0400

    updated restaurant.sql with thumbnail_photos

--- db/seeds/02_restaurants.sql
+++ db/seeds/02_restaurants.sql
@@ -1,11 +1,17 @@
-INSERT INTO restaurants (name, description, opening_hours, website, phone_number, address) VALUES 
-('Embassy Restaurant', 'South Asian Tasty Dishes', '10am to 10pm MON-SUN', 'www.embassyrestaurant.com', '647-637-6969', '245 Mc Nicol Ave, Scarborough, ON');
+INSERT INTO restaurants (name, thumbnail_photo , description, opening_hours, website, phone_number, address) VALUES 
+('Embassy Restaurant', './public/thumbnail_photos/Embassy.png', 'South Asian Tasty Dishes', '10am to 10pm MON-SUN', 'www.embassyrestaurant.com'./public/thumbnail_photos/image_01.png, '647-637-6969', '245 Mc Nicol Ave, Scarborough, ON');
 
-INSERT INTO restaurants (name, description, opening_hours, website, phone_number, address) VALUES 
-('Mc Donalds Restaurant', 'Fast food restaurant', '8am to 11pm MON-SUN', 'www.mcdonalds.com', '647-637-6969', '116 Warden Ave E, Vancouver, ON');
+INSERT INTO restaurants (name, thumbnail_photo , description, opening_hours, website, phone_number, address) VALUES 
+('Mc Donalds Restaurant', './public/thumbnail_photos/McD.png', 'Fast food restaurant', '8am to 11pm MON-SUN', 'www.mcdonalds.com', '647-637-6969', '116 Warden Ave E, Vancouver, ON');
 
-INSERT INTO restaurants (name, description, opening_hours, website, phone_number, address) VALUES 
-('Mandarin Restaurant', 'Chinese Canadian Buffet', '10.30am to 10.30pm MON-SUN', 'www.mandarin.com', '647-637-6969', '2001 Birchmount Rd N, Toronto, ON');
+INSERT INTO restaurants (name, thumbnail_photo , description, opening_hours, website, phone_number, address) VALUES 
+('Mandarin Restaurant', './public/thumbnail_photos/Mandarin.png', 'Chinese Canadian Buffet', '10.30am to 10.30pm MON-SUN', 'www.mandarin.com', '647-637-6969', '2001 Birchmount Rd N, Toronto, ON');
 
-INSERT INTO restaurants (name, description, opening_hours, website, phone_number, address) VALUES 
-('Swiss Chalet Restaurant', 'Swiss Chalet is Canada's favourite place for rotisserie chicken', '11.00 am to 11.00 pm MON-SUN', 'www.swisschalet.com', '647-637-6969', '1993 Salem Rd N, Ajax, ON');
+INSERT INTO restaurants (name, thumbnail_photo , description, opening_hours, website, phone_number, address) VALUES 
+('Swiss Chalet Restaurant', './public/thumbnail_photos/Swiss_chalet.png', 'Swiss Chalet is Canada's favourite place for rotisserie chicken', '11.00 am to 11.00 pm MON-SUN', 'www.swisschalet.com', '647-637-6969', '1993 Salem Rd N, Ajax, ON');
+
+INSERT INTO restaurants (name, thumbnail_photo , description, opening_hours, website, phone_number, address) VALUES 
+('Starbucks Coffee', './public/thumbnail_photos/Star_bucks.png', 'Canadian Coffee Shop', '10.30am to 10.30pm MON-SUN', 'www.starbucks.com', '647-637-6969', '8475 Lawrence Rd N, Toronto, ON');
+
+INSERT INTO restaurants (name, thumbnail_photo , description, opening_hours, website, phone_number, address) VALUES 
+('Kama Restaurant', './public/thumbnail_photos/Kama.png', 'Kama is Canada's favourite place for South Asian Dishes', '11.00 am to 11.00 pm MON-SUN', 'www.kama.com', '647-637-6969', '9651 Westney Rd N, Ajax, ON');
